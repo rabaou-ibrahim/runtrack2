d634a07c941496eb4e971abdf3bf7e20e54686e4
rabaou-ibrahim <114422669+rabaou-ibrahim@users.noreply.github.com>
2022-11-28 09:29:11 +0100

    Add files via upload

---
+++ jour10/job01/job01.php
@@ -0,0 +1,19 @@
+<?php
+
+$mysqlConnection = new PDO(
+    'mysql:host=localhost;dbname=jour09;charset=utf8',
+    'root',
+    ''
+);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php echo "job01" ?></title>
+</head>
+ 
+<body>
+
+    <thead></thead>
+
